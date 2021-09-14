fff82f34537fa3198bfbbcfa6bc0b0b4a4199f8a
Jugep001 <juliang1@live.de>
2021-09-14 18:45:45 +0200

    - added Discord in social - changed uptime, so the user can change it

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ var Wer = '';
 var Game = '';
 var Games = '';
 var Death = '';
+var Scare = '';
+var Uptime = '';
 
 //set env
 process.env.NODE_ENV = 'production';
 
 let mainWIndow;
+let corrected_hours = 0;
 
 
 //toDate function
@@ -46,7 +49,7 @@ function gameRequest(accessToken){
             'Client-ID': process.env.CLIENT_ID,
             'Authorization': 'Bearer ' + accessToken
         }
-    }
+    };
     if(!accessToken){
         console.log("No Token");
     }else{
@@ -56,7 +59,7 @@ function gameRequest(accessToken){
         if(err){
             return console.log(err);
         }
-        
+        console.log(body);
         const json_obj = JSON.parse(body);
         if(json_obj.data[0] !== undefined){
             uptime = json_obj.data[0].started_at;
@@ -64,9 +67,9 @@ function gameRequest(accessToken){
         
     });
     
-    };
+    }
     
-    },2000)
+    },2000);
     }
 const options = {
     url: 'https://id.twitch.tv/oauth2/token',
@@ -131,7 +134,7 @@ ipcMain.on('Elo:add', function(e, Elo_value){
 ipcMain.on('Lp:add', function(e, Lp_value){
 
     LP = Lp_value;
-    addToJson();
+    addToJson("Elo");
 
 });
 
@@ -156,26 +159,60 @@ ipcMain.on('Games:add', function(e, Games_Value){
 
 });
 
-// Catch Games:add
+// Catch Death:add
 ipcMain.on('Death:add', function(e, Death_Value){
     
     Death = Death_Value;
 
 });
 
-//Append to Json
-function addToJson(){
-    var obj = {
-        Elo: Elo,
-        LP: LP
-     };
+// Catch Scare:add
+ipcMain.on('Scare:add', function(e, Scare_Value){
     
-            var json = JSON.stringify(obj); 
-            fs.writeFile('Elo.json', json, 'utf8', function(err) {
-                if (err) throw err;
-                console.log('complete');
-                }
-            ); 
+    Scare = Scare_Value;
+
+});
+
+// Catch Uptime:add
+ipcMain.on('Uptime:add', function(e, Uptime_Value){
+    
+    Uptime = Uptime_Value;
+    addToJson("Uptime");
+
+});
+//Append to Json
+function addToJson(mode){
+
+
+    if(mode === "Elo"){
+        var obj = {
+            Elo: Elo,
+            LP: LP
+         };
+        
+                var json = JSON.stringify(obj); 
+                fs.writeFile('Elo.json', json, 'utf8', function(err) {
+                    if (err) throw err;
+                    console.log('complete');
+                    }
+                ); 
+    }
+
+    else if(mode === "Uptime"){
+        console.log(Uptime);
+        var obj2 = {
+            Uptime: Uptime,
+         };
+        
+                var json2 = JSON.stringify(obj2); 
+                fs.writeFile('Uptime.json', json2, 'utf8', function(err) {
+                    if (err) throw err;
+                    console.log('complete');
+                    }
+                ); 
+
+    }
+    
        
      
      
@@ -244,7 +281,7 @@ const Botoptions = {
         password: process.env.TWITCH_OATH_TOKEN,
         
     },
-    channels: ['ayseatv']
+    channels: ['Megahero1997']
 };
 
 const client = new tmi.client(Botoptions);
@@ -256,40 +293,49 @@ client.on('connected', (address, port) => {
     
 });
 
+setInterval(function(){
+    
+        client.action('Megahero1997', `Mit dem Befehl "!help" könnt ihr meine Befehle sehen!`);
+    
+     }, 1200000); //600000 für 10 Minuten
+
 client.on('chat', (channel, user, message, self) => {
     switch (message){
     case '!wer':
         if(Wer === ''){
-            client.action('ayseatv', `Aysea spielt mit niemanden`);
+            client.action('Megahero1997', `Aysea spielt mit niemanden`);
         }
         else{
-            client.action('ayseatv', `Aysea spielt mit ${Wer}`);
+            client.action('Megahero1997', `Aysea spielt mit ${Wer}`);
         }
         
         break;
         
     case '!game':
         if(Game === ''){
-            client.action('ayseatv', `Aysea spielt gerade nichts`);
+            client.action('Megahero1997', `Aysea spielt gerade nichts`);
         }
         else{
-            client.action('ayseatv', `Aysea spielt gerade ${Game}`);
+            client.action('Megahero1997', `Aysea spielt gerade ${Game}`);
         }
         
         break;
     case '!upcoming':
         if(Games === ''){
 
-            client.action('ayseatv', `Aysea spielt heute nichts`);
+            client.action('Megahero1997', `Aysea spielt heute nichts`);
             
         }
         else{
-            client.action('ayseatv', `Aysea spielt heute ${Games}`);
+            client.action('Megahero1997', `Aysea spielt heute ${Games}`);
         }
             
         break;    
     case '!uptime':
-
+        
+        
+    
+        
         console.log(uptime);
 
         let time = new Date();
@@ -297,47 +343,98 @@ client.on('chat', (channel, user, message, self) => {
         let uptime_date = new Date(uptime);
         let uptime_difference = time.getTime() - uptime_date.getTime();
         let uptime_difference_date = new Date(uptime_difference);
-        let hours = uptime_difference_date.getHours();
+        let hours = uptime_difference_date.getHours() - 1;
         let minutes = uptime_difference_date.getMinutes();
         let seconds = uptime_difference_date.getSeconds();
+
         
-        if(!Number.isNaN(hours)){
-            client.action('ayseatv', `Aysea ist seit ${hours} Stunden ${minutes} Minuten und ${seconds} Sekunden on`);
+       
+        if(!Number.isNaN(hours))
+        {
+            fs.readFile('Uptime.json', 'utf8', function readFileCallback(err, data)
+            {
+                if (err)
+                {
+                    alert("Fehlende Datei!");
+                } 
+                
+                else 
+                {
+                    var Uptime_object = JSON.parse(data); //put Uptime data in 
+                    switch(Uptime_object.Uptime)
+                    {
+                        case '+1':
+                            corrected_hours = hours + 1;
+                            client.action('Megahero1997', `Aysea ist seit ${corrected_hours} Stunden ${minutes} Minuten und ${seconds} Sekunden on`);
+                            break;
+                        case '-1':
+                            corrected_hours = hours - 1;
+                            client.action('Megahero1997', `Aysea ist seit ${corrected_hours} Stunden ${minutes} Minuten und ${seconds} Sekunden on`);
+                            break;
+                        case 'Default':
+                            corrected_hours = hours;
+                            client.action('Megahero1997', `Aysea ist seit ${corrected_hours} Stunden ${minutes} Minuten und ${seconds} Sekunden on`);
+                            break;
+                        default:
+                            break;
+                    }
+                } 
+            }); 
+            
             break;
         }
         else{
-            client.action('ayseatv', `Aysea ist gerade nicht on`);
+            client.action('Megahero1997', `Aysea ist gerade nicht on`);
             break;
         }
         break;
+
     case '!elo':
         fs.readFile('Elo.json', 'utf8', function readFileCallback(err, data){
             if (err){
                 alert("Fehlende Datei!");
             } else {
             var Elo_object = JSON.parse(data); //now it an object
-            client.action('ayseatv', `Aysea ist momentan ${Elo_object.Elo} mit ${Elo_object.LP} LP!`); 
+            client.action('Megahero1997', `Aysea ist momentan ${Elo_object.Elo} mit ${Elo_object.LP} LP!`); 
         }});
         break;
     case '!social':
-        var youtube = "YOUTUBE:https://www.youtube.com/channel/UCC-TbbEnKvFLVvcUyEvUn_w";
-        var instagram = "INSTAGRAM:https://www.instagram.com/ayseax3/";
-        var twitter = "TWITTER:https://twitter.com/Aysea_TV";
-        client.action('ayseatv', `${youtube}`);
-        client.action('ayseatv', `${instagram}`);
-        client.action('ayseatv', `${twitter}`);
+        var youtube = "YOUTUBE: https://www.youtube.com/channel/UCC-TbbEnKvFLVvcUyEvUn_w";
+        var instagram = "INSTAGRAM: https://www.instagram.com/ayseax3/";
+        var twitter = "TWITTER: https://twitter.com/Aysea_TV";
+        var discord = "DISCORD: https://discord.gg/zVqqE3Mm3W";
+        client.action('Megahero1997', `${youtube}`);
+        client.action('Megahero1997', `${instagram}`);
+        client.action('Megahero1997', `${twitter}`);
+        client.action('Megahero1997', `${discord}`);
         break;
 
     case '!deaths':
         if(Death === ''){
-            client.action('ayseatv', `Aysea ist schon 0 mal gestorben!`); 
+            client.action('Megahero1997', `Aysea ist schon 0 mal gestorben!`); 
         }
         else{
-            client.action('ayseatv', `Aysea ist schon ${Death} mal gestorben!`); 
+            client.action('Megahero1997', `Aysea ist schon ${Death} mal gestorben!`); 
         }
         
         break;
+    case '!jumpscares':
+        if(Scare === ''){
+            client.action('Megahero1997', `Aysea hat sich 0 mal erschrocken!`); 
+        }
+        else{
+            client.action('Megahero1997', `Aysea hat sich ${Scare} mal erschrocken! Was nen Pleb!`); 
+        }
+            
+        break;
+    case '!noob':
+        client.action('Megahero1997', `Aysea ist schon nen richtiger Noob xD`);
+
+        break;
+    case '!help':
+        client.action('Megahero1997', `Die folgenden Commands gibt es: !wer, !game, !upcoming, !uptime, !elo, !social, !deaths, !jumpscares`); 
         
+        break;
     default:
         break;
     }
